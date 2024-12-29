c1e199dd0b2e921c813d643f1180a5da13600a60
fadingduck00 <azizaagayeva220@gmail.com>
2024-12-29 17:10:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,31 @@
 print('hello world')
 print("2+2")
+x = 1 
+y = 'hello'
+print (x)
+print(y)
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+x = 5
+y = 3
+print(x + y)
+x = 5
+y = 3
+print(x - y)
+x = 5
+
+print(x)
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
